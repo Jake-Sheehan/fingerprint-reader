145a8c61e120d1893bdfda18fb1f30274c694352
Jake Sheehan <jake.sheehan.dev@gmail.com>
2024-06-20 11:15:43 -0400

    added Fingerprint type

---
+++ src/fingerprint.rs
@@ -0,0 +1,95 @@
+use std::fmt;
+use std::fs;
+
+#[allow(dead_code)]
+#[derive(Debug)]
+pub struct Fingerprint {
+    data: Vec<Vec<char>>,
+    name: String,
+    year: u32,
+    rows: usize,
+    cols: usize,
+}
+
+#[allow(dead_code)]
+impl Fingerprint {
+    fn read_file_lines(filename: &str) -> Vec<String> {
+        let uri = format!("data/{}", filename);
+        fs::read_to_string(uri)
+            .expect("error: failed to read file")
+            .lines()
+            .map(String::from)
+            .collect()
+    }
+
+    pub fn new(filename: &str) -> Self {
+        let lines = Self::read_file_lines(filename);
+        let name = lines[0].trim();
+        let name: String = name.to_string();
+        let year: u32 = lines[1].trim().parse().unwrap();
+        let rows: usize = lines[2].trim().parse().unwrap();
+        let cols: usize = lines[3].trim().parse().unwrap();
+        let data: Vec<Vec<char>> = lines
+            .iter()
+            .skip(4)
+            .map(|line| line.chars().collect::<Vec<char>>())
+            .collect();
+
+        return Self {
+            data,
+            name,
+            year,
+            rows,
+            cols,
+        };
+    }
+
+    pub fn get_number_of_pixels(&self) -> usize {
+        self.rows * self.cols
+    }
+
+    pub fn print_image(&self) {
+        for line in &self.data {
+            for c in line {
+                print!("{}", c);
+            }
+            print!("\n");
+        }
+    }
+}
+
+impl fmt::Display for Fingerprint {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "Fingerprint for: {}. Year registered: {}. Number of pixels: {}",
+            self.name,
+            self.year,
+            self.get_number_of_pixels()
+        )
+    }
+}
+
+impl PartialEq for Fingerprint {
+    fn eq(&self, other: &Self) -> bool {
+        if self.name != other.name
+            || self.year != other.year
+            || self.rows != other.rows
+            || self.cols != other.cols
+        {
+            return false;
+        }
+
+        for row in 0..self.rows {
+            for col in 0..self.cols {
+                if self.data[row][col] != other.data[row][col] {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
+
+impl Eq for Fingerprint {}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+mod fingerprint;
+
+use fingerprint::Fingerprint;
+
 fn main() {
-    println!("Hello, world!");
+    let original = Fingerprint::new("original.txt");
+    println!("{}", original);
+    original.print_image();
+}
+
+// ----- TESTS -----
+
+#[test]
+fn fingerprint_equality() {
+    let fingerprint1 = Fingerprint::new("original.txt");
+    let fingerprint2 = Fingerprint::new("original.txt");
+    let fingerprint3 = Fingerprint::new("original.txt");
+
+    assert_eq!(fingerprint1, fingerprint2);
+    assert_eq!(fingerprint2, fingerprint1);
+    assert_eq!(fingerprint2, fingerprint3);
+    assert_eq!(fingerprint1, fingerprint3);
 }
